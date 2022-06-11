6ceff5f1bbb2f9d19be0bb2b8825f45b2159927e
cunp <simizu1251@gmail.com>
2022-06-11 12:24:56 +0900

    feat:평균구하기3

--- jaehoon/평균구하기.java
+++ jaehoon/평균구하기.java
@@ -1,9 +1,12 @@
 package jaehoon;
 
-class Solution1 {
-    public int[] solution(long n) {
-        int[] answer = {};
+class 평균구하기 {
+    public double solution(int[] arr) {
+        double answer = 0;
+        for(int i = 0; i < arr.length; i++) {
+            answer += arr[i];  //모든 숫자의 합
+        }
+        answer = answer / arr.length;
         return answer;
-
     }
 }
