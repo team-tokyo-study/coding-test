e714fba1088297da01617a957e67feba5adf510d
sangwoo <dev.sangwoo@gmail.com>
2022-06-15 18:33:36 +0900

    Feat : 1week algorithms

---
+++ sangwoo/뒤집은자연수.java
@@ -0,0 +1,15 @@
+class Reversed {
+    public int[] solution(long n) {
+          String s = String.valueOf(n); //String.valueOf를 통하여 Long 자료형을 String으로 변환
+        int[] answer = new int[s.length()]; //변환한 문자열 길이 만큼 int[] 배열을 선언하여 초기화
+        
+        for(int i=0; i<s.length(); i++) {
+            int value = Integer.parseInt(s.substring(s.length()-i-1, s.length()-i));
+            System.out.println(s.substring(4,5));
+            System.out.println(s.length()-i-1);
+            System.out.println(s.length()-i);
+            answer[i] = value;
+        }
+        return answer;
+    }
+}
