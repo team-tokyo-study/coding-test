4a78c4e1c913010b5bae9864109447adbabfd79b
KyungJoon960 <kjkim77960@gmail.com>
2022-06-15 14:51:46 +0900

    Average(if, while)

---
+++ kj/평균구하기 while.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        double sum = 0;
+        
+        int i = 0;
+        
+        while(i < arr.length){
+            sum = sum + arr[i];
+            i++;
+            
+        }
+        
+        answer = sum / arr.length;
+        
+        return answer;
+    }
+}
